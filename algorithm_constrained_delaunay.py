d80f0965870fae9e68204fc0da482a1601b946f5
Olivier Dalang <olivier.dalang@epfl.ch>
2014-11-10 10:56:14 +0100

    restricted delaunay works (it seems)

--- algorithm_constrained_delaunay.py
+++ algorithm_constrained_delaunay.py
@@ -54,61 +54,70 @@ def computeConstrainedDelaunayTriangulation(points, constraints):
 
             QgsMessageLog.logMessage("Segment is %s" % str(segment))
 
-            trianglesToRemove = set() # This will store the triangles that must be deleted
+            trianglesToRemove = [] # This will store the triangles that must be deleted
 
             # We check for intersection 
             for triangle in triangles:
                 for edge in [ [triangle[0],triangle[1]], [triangle[1],triangle[2]], [triangle[2],triangle[0]] ]:
                     if intersects(segment, edge):
                         QgsMessageLog.logMessage("We must remove %s" % str(triangle) )
-                        trianglesToRemove.add(triangle) # If there is an intersection between the segment and an edge of the triangle, we have to remove it
+                        trianglesToRemove.append(triangle) # If there is an intersection between the segment and an edge of the triangle, we have to remove it
                         break
 
 
             # Now we're going to remove all the triangles, and retriangulate the left and the right part of the segment
+            if len(trianglesToRemove)>0:
 
-            pointsLeft = segment[:] # This will hold the list of points IDs consituing the part to be retriangulated on the left side of the segment
-            pointsRight = segment[:] # This will hold the list of points IDs consituing the part to be retriangulated on the right side of the segment
+                pointsLeft = set() # This will hold the list of points IDs consituing the part to be retriangulated on the left side of the segment
+                pointsRight = set() # This will hold the list of points IDs consituing the part to be retriangulated on the right side of the segment
 
-            for triToRemove in trianglesToRemove:
+                pointsLeft.add(segment[0])
+                pointsLeft.add(segment[1])
+                pointsRight.add(segment[0])
+                pointsRight.add(segment[1])
 
-                QgsMessageLog.logMessage("We remove %s" % str(triToRemove))
-                triangles.remove( triToRemove ) # We remove the triangles
+                QgsMessageLog.logMessage("%s was added to the left as part of the segment" % str(pointsLeft))
+                QgsMessageLog.logMessage("%s was added to the right as part of the segment" % str(pointsRight))
 
-                for pID in triToRemove: # And for each poiont
-                    if pID != segment[0] and pID != segment[1]:
-                        # We add it to 
+                for triToRemove in trianglesToRemove:
+
+                    QgsMessageLog.logMessage("We remove %s" % str(triToRemove))
+                    triangles.remove( triToRemove ) # We remove the triangles
+
+                    for pID in triToRemove: # And for each poiont
                         if isPointLeftOfRay( points[pID], points[segment[0]], points[segment[1]]):
-                            pointsLeft.append(pID)
+                            pointsLeft.add(pID)
                             QgsMessageLog.logMessage("%i was added to the left" % pID)
                         else:
-                            pointsRight.append(pID)
+                            pointsRight.add(pID)
                             QgsMessageLog.logMessage("%i was added to the right" % pID)
 
-                
-            QgsMessageLog.logMessage("Pointsleft is %s" % str(pointsLeft) )
-            QgsMessageLog.logMessage("Pointsright is %s" % str(pointsRight) )
+                    
+                QgsMessageLog.logMessage("Pointsleft is %s" % str(pointsLeft) )
+                QgsMessageLog.logMessage("Pointsright is %s" % str(pointsRight) )
 
-            # We compute the delaunay triangulation for the left side
-            trianglesLeft = voronoi.computeDelaunayTriangulation( [voronoi.Site(points[pID].x(), points[pID].y()) for pID in pointsLeft] )
-            QgsMessageLog.logMessage("%i triangles computed for the left (%s)" % (len(trianglesLeft),trianglesLeft))  
-            for tri in trianglesLeft:
-                # And map the triangles point indices to the actual point indices
-                mappedTri = [ pointsLeft[tri[0]],pointsLeft[tri[1]],pointsLeft[tri[2]] ]
-                # And add the triangles to the list
-                QgsMessageLog.logMessage("%s triangles added for the left" % str(mappedTri) )  
-                triangles.append( mappedTri )
+                # We compute the delaunay triangulation for the left side
+                arrayLeft = list(pointsLeft)
+                trianglesLeft = voronoi.computeDelaunayTriangulation( [voronoi.Site(points[pID].x(), points[pID].y()) for pID in pointsLeft] )
+                QgsMessageLog.logMessage("%i triangles computed for the left (%s)" % (len(trianglesLeft),trianglesLeft))  
+                for tri in trianglesLeft:
+                    # And map the triangles point indices to the actual point indices
+                    mappedTri = [ arrayLeft[tri[0]],arrayLeft[tri[1]],arrayLeft[tri[2]] ]
+                    # And add the triangles to the list
+                    QgsMessageLog.logMessage("%s triangles added for the left" % str(mappedTri) )  
+                    triangles.append( mappedTri )
 
 
-            # We compute the delaunay triangulation for the right side
-            trianglesRight = voronoi.computeDelaunayTriangulation( [voronoi.Site(points[pID].x(), points[pID].y()) for pID in pointsRight] )
-            QgsMessageLog.logMessage("%i triangles computed for the right (%s)" % (len(trianglesRight),trianglesRight))  
-            for tri in trianglesRight:
-                # And map the triangles point indices to the actual point indices
-                mappedTri = [ pointsRight[tri[0]],pointsRight[tri[1]],pointsRight[tri[2]] ]
-                # And add the triangles to the list
-                QgsMessageLog.logMessage("%s triangles added for the right" % str(mappedTri) ) 
-                triangles.append( mappedTri )
+                # We compute the delaunay triangulation for the right side
+                arrayRight = list(pointsRight)
+                trianglesRight = voronoi.computeDelaunayTriangulation( [voronoi.Site(points[pID].x(), points[pID].y()) for pID in arrayRight] )
+                QgsMessageLog.logMessage("%i triangles computed for the right (%s)" % (len(trianglesRight),trianglesRight))  
+                for tri in trianglesRight:
+                    # And map the triangles point indices to the actual point indices
+                    mappedTri = [ arrayRight[tri[0]],arrayRight[tri[1]],arrayRight[tri[2]] ]
+                    # And add the triangles to the list
+                    QgsMessageLog.logMessage("%s triangles added for the right" % str(mappedTri) ) 
+                    triangles.append( mappedTri )
 
 
     return triangles
